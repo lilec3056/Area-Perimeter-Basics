b24e8723ed9b8398cb487536773f6f313a6640ec
lilec3056 <lilec3056@masseyhigh.school.nz>
2024-05-29 14:54:48 +1200

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,22 @@
+
+
+
+
+# variables lowercase underscore
+greeting = "hello world"
+
+fav_number = 27
+
+love_programing = True
+
+num_string = "Hello "
+
+print(fav_number * 2)
+print(num_string * 10)
+
+
+# upper case constant
+PI = 3.14
+
+# needs letter before a number in the name of a variable
+a_01_my_num = 4
